b0b545f6e9a0153e7bcc917e30108d6b64025c64
Sami Hamici <sami.hamici@reski.fi>
2019-08-24 10:22:25 +0200

    First commit

---
+++ Timer_test.py
@@ -0,0 +1,31 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+import threading
+from detectors import yolo_detect, color_detect, movement_detect
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--mode", required=True, help="Start or end gate")
+ap.add_argument("-i", "--input", required=True, help="Select input")
+ap.add_argument("-t", "--type", required=True, help="Set detection type")
+ap.add_argument("-r", "--rotate", required=False, help="Rotate image")
+args = vars(ap.parse_args())
+
+if args["type"] == "yolo":
+	#thread1 = threading.Thread(target = yolo_detect, args = (args["input"], args["mode"]))
+	#thread1.start()
+	yolo_detect(args["input"], args["mode"])
+if args["type"] == "color":
+	color_detect(args["input"], args["mode"])
+if args["type"] == "movement":
+	if args["rotate"] == "true":
+		movement_detect(args["input"], args["mode"], True)
+	else :
+		movement_detect(args["input"], args["mode"], False)
+#thread2.start()
+#
+#yolo_detect("videos/end.mp4", "end")
---
+++ detectors.py
@@ -0,0 +1,299 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+import array as arr
+
+# Find frames
+def movement_detect(video_path, video_type, rotate):
+	object_treshold = 0.10
+	first_run = 0
+	cap = cv2.VideoCapture(video_path)
+	x1 = 1
+	x2 = 20
+	y1 = 100
+	y2 = 200
+	captured_pixels = 0
+	changed_pixels = 0
+	detection_on = False
+	frames_detected = 0
+	detector_lock = False
+	timer_running = False
+
+	while True:
+		(grabbed, frame) = cap.read()
+		# if the frame was not grabbed, then we have reached the end
+		# of the stream
+		if not grabbed:
+			video_path = "videos/end_sample.mp4"
+			x1 = 1
+			x2 = 20
+			y1 = 100
+			y2 = 200
+			captured_pixels = 0
+			changed_pixels = 0
+			detection_on = False
+			frames_detected = 0
+			detector_lock = False
+			first_run = 0
+			cap = cv2.VideoCapture(video_path)
+			continue
+		if rotate:
+			rows,cols = frame.shape[:2]
+
+			M = cv2.getRotationMatrix2D((cols/2,rows/2),270,1)
+			frame = cv2.warpAffine(frame,M,(cols,rows))
+			original = frame.copy()
+
+		if first_run < 2:
+			(h, w) = frame.shape[:2]
+			video_width = w
+			video_height = h
+			pixel_memory = np.zeros((int(video_height), int(video_width), 3), np.uint8)
+			first_run = first_run + 1
+			x1 = int(w/2) - 10
+			x2 = int(w/2) + 10
+			y1 = 100
+			y2 = int(h)
+
+		if detection_on:
+			for x in range(x1, x2, 5):
+				for y in range(y1, y2, 5):
+					for d in range(3) :
+						dd = pixel_memory[y][x][d]
+						dd2 = frame[y][x][d]
+						if ((dd + 20) < dd2 ) or ((dd - 20) > dd2):
+							changed_pixels = changed_pixels + 1
+
+			changed_pixels = changed_pixels * 5*5
+			#print("Changed pixels "+ str(changed_pixels))
+			#print("captured pixels" + str(captured_pixels))
+			#print("Division " + str(changed_pixels/captured_pixels))
+			if(detector_lock and (time.time() > (detector_delay + 20))):
+				detector_lock = False
+				print("detector_lock released")
+
+			if( ((changed_pixels/captured_pixels) > object_treshold) and (detector_lock is False) ):
+				frames_detected = frames_detected + 1
+				if(frames_detected > 2):
+					if(timer_running):
+						print("Person detected, Stopping timer")
+						frames_detected = 0
+						#Get clock
+						detector_delay = time.time()
+						detector_lock = True
+						print("Captured time: " + str(time.time() - rider_time) + " seconds")
+						timer_running = False
+						cv2.waitKey(1000)
+						return;
+					else:
+						print("Person detected, Starting timer")
+						frames_detected = 0
+						#Get clock
+						detector_delay = time.time()
+						detector_lock = True
+						rider_time = time.time()
+						timer_running = True
+			else:
+				frames_detected = 0
+
+		cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3 )
+		display = cv2.resize(frame, (int(w * 0.4),int(h * 0.4)))
+		cv2.imshow('frame', display)
+		#cv2.imshow('original', original)
+
+		changed_pixels = 0
+		k = cv2.waitKey(10) & 0xFF
+		if k == ord('s'):
+			# Store pixels from rectangle area
+			if rotate == False:
+				 (grabbed, original) = cap.read()
+			(h, w) = original.shape[:2]
+			x1 = int(w/2) - 10
+			x2 = int(w/2) + 10
+			y1 = 100
+			y2 = int(h)
+			for x in range(x1, x2):
+				for y in range(y1, y2):
+					for d in range(3) :
+						pixel_memory[y][x][d] = original[y][x][d]
+						captured_pixels = captured_pixels + 1
+					#print(pixel_memory[x][y])
+			#cv2.imshow("stored frame", pixel_memory)
+			print("Starting detection, captured pixels: " + str(captured_pixels))
+			detection_on = True
+		if k == 27:
+			break
+
+# Color detector
+def color_detect(video_path, video_type):
+	cap = cv2.VideoCapture(0)
+
+	while True:
+		(grabbed, frame) = cap.read()
+		# if the frame was not grabbed, then we have reached the end
+		# of the stream
+		if not grabbed:
+			break
+
+		video_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+		video_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+		#Convert images from BGR to HSV
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		lower_red = np.array([110, 50, 50])
+		upper_red = np.array([130, 255, 255])
+
+		# Here we are defining range of bluecolor in hsv
+		# This creates a mask of blue coloured
+		# Objects found in the frame
+		mask = cv2.inRange(hsv, lower_red, upper_red)
+
+		# The bitwise and of the frame and mask is done so
+		# that only the blue coloured objects are highlighted
+		# and stored in res
+		res = cv2.bitwise_and(frame,frame, mask= mask)
+		cv2.line(frame, (320, 1), (320, int(video_height)), (0, 255, 0), 2)
+		cv2.imshow('frame',frame)
+		cv2.imshow('mask',mask)
+		cv2.imshow('res',res)
+		k = cv2.waitKey(5) & 0xFF
+		if k == 27:
+			break
+# Yolo
+def_confidence = 2
+def_threshold = 2
+
+def yolo_detect(video_path, video_type):
+# load the COCO class labels our YOLO model was trained on
+	labelsPath = os.path.sep.join(["yolo_files", "coco.names"])
+	LABELS = open(labelsPath).read().strip().split("\n")
+
+	# initialize a list of colors to represent each possible class label
+	np.random.seed(42)
+	COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+
+	# derive the paths to the YOLO weights and model configuration
+	weightsPath = os.path.sep.join(["yolo_files/", "yolov3.weights"])
+	configPath = os.path.sep.join(["yolo_files", "yolov3.cfg"])
+
+	# load our YOLO object detector trained on COCO dataset (80 classes)
+	# and determine only the *output* layer names that we need from YOLO
+	net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+	ln = net.getLayerNames()
+	ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+	# initialize the video stream, pointer to output video file, and
+	# frame dimensions
+	vs = cv2.VideoCapture(video_path)	# Read video
+
+	# Get dimensions from video start footage
+	video_width = vs.get(cv2.CAP_PROP_FRAME_WIDTH)
+	video_height = vs.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+	writer = None
+	(W, H) = (None, None)
+
+	Detection_count = 1
+	Process_frame = 10
+	frame_count = 0
+
+	block_gate = False
+	timer_running = False
+
+	while True:
+		# read the next frame from the file
+		(grabbed, frame) = vs.read()
+		frame_count = frame_count + 1
+		if(frame_count >= Process_frame):
+			# if the frame was not grabbed, then we have reached the end
+			# of the stream
+			if not grabbed:
+				break
+
+			# if the frame dimensions are empty, grab them
+			if W is None or H is None:
+				(H, W) = frame.shape[:2]
+			blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+			net.setInput(blob)
+
+			layerOutputs = net.forward(ln)
+			end = time.time()
+
+			# initialize our lists of detected bounding boxes, confidences,
+			# and class IDs, respectively
+			boxes = []
+			confidences = []
+			classIDs = []
+
+			# loop over each of the layer outputs
+			for output in layerOutputs:
+				# loop over each of the detections
+				for detection in output:
+					# extract the class ID and confidence (i.e., probability)
+					# of the current object detection
+					scores = detection[5:]
+					classID = np.argmax(scores)
+					confidence = scores[classID]
+					# filter out weak predictions by ensuring the detected
+					# probability is greater than the minimum probability
+					if confidence > def_confidence:
+						# scale the bounding box coordinates back relative to
+						# the size of the image, keeping in mind that YOLO
+						# actually returns the center (x, y)-coordinates of
+						# the bounding box followed by the boxes' width and
+						# height
+						box = detection[0:4] * np.array([W, H, W, H])
+						(centerX, centerY, width, height) = box.astype("int")
+						# use the center (x, y)-coordinates to derive the top
+						# and and left corner of the bounding box
+						x = int(centerX - (width / 2))
+						y = int(centerY - (height / 2))
+
+						# update our list of bounding box coordinates,
+						# confidences, and class IDs
+						boxes.append([x, y, int(width), int(height)])
+						confidences.append(float(confidence))
+						classIDs.append(classID)
+
+			# apply non-maxima suppression to suppress weak, overlapping
+			# bounding boxes
+			idxs = cv2.dnn.NMSBoxes(boxes, confidences, def_confidence, def_threshold)
+			if(block_gate and ( (time.time() - block_gate_timer) > 5)) :
+				block_gate = False
+			# ensure at least one detection exists
+			if (len(idxs) > 0 and (block_gate == False)):
+				# loop over the indexes we are keeping
+				for i in idxs.flatten():
+					# extract the bounding box coordinates
+					(x, y) = (boxes[i][0], boxes[i][1])
+					(w, h) = (boxes[i][2], boxes[i][3])
+					Detection_count = Detection_count + 1
+					if (x < W/2) and ( (x + w) > W/2):
+						if(timer_running == False) :
+							print("Person crossed start ", Detection_count)
+							start = time.time()
+							block_gate = True
+							block_gate_timer = time.time();
+							print("Timer started")
+							timer_running = True
+						else:
+							timer_running = False
+							print("Timer stopped")
+							stop = time.time();
+							elap = (stop - start)
+							print("Time elapsed {:.4f} seconds".format(elap))
+							block_gate = True
+							block_gate_timer = time.time();
+					print("")
+
+					# Detect if person collides wit start line
+		cv2.line(frame, (320, 1), (320, int(video_height)), (0, 255, 0), 2)
+		cv2.imshow(video_type, frame)
+		cv2.waitKey(1)
+			# release the file pointers
+	print("[INFO] cleaning up...")
+	writer.release()
+	vs.release()
